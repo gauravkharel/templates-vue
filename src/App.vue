18b8758a9686806ab8fd95ae46c9a63a3b521835
gauravkharel <kgaurav21@tbc.edu.np>
2022-06-27 15:21:04 +0545

    first commit

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    helor
+    ckanonajcoinao
+    <HelloWorld />
+    <HockeyTeamList />
+  </div>
+
+</template>
+
+<script>
+import HelloWorld from "./components/HelloWorld.vue";
+import HockeyTeamList from "./components/HockeyTeamList.vue";
+export default {
+  components: { HelloWorld, HockeyTeamList },
+  setup() {
+    return {};
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+
+</style>
